aace23b1981c234609dffaafd9505011a8f280ed
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-12-24 15:45:57 +0300

    commit

--- err_msgs.c
+++ err_msgs.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   err_msgs.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/20 20:42:29 by aozkaya           #+#    #+#             */
-/*   Updated: 2024/12/23 17:48:54 by aabdulmecit      ###   ########.fr       */
+/*   Created: 2024/12/23 14:31:14 by aabdulmecit       #+#    #+#             */
+/*   Updated: 2024/12/23 18:10:12 by aabdulmecit      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# define GREEN "\033[0;32m"
-# define RED "\033[1;31m"
-# define GREY "\033[0;90m"
-# define CYAN "\033[1;96m"
-# define RESET "\033[0m"
-
-# include "./lib/libft/libft.h"
-
-void    err(char *msg);
-
-
-
-#endif
+void    err(char *msg)
+{
+    ft_printf(RED "\n%s\n" RESET, msg);
+}
