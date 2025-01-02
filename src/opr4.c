79f8b1e6fdf903be99291de4fbeb1b563e9b03cd
Ahmet abdülmecit Özkaya <aozkaya@k1m10s09.42istanbul.com.tr>
2025-01-02 21:12:39 +0300

    commit

--- src/opr4.c
+++ src/opr4.c
@@ -6,23 +6,43 @@
 /*   By: aozkaya <aozkaya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 19:13:31 by aozkaya           #+#    #+#             */
-/*   Updated: 2025/01/02 19:14:37 by aozkaya          ###   ########.fr       */
+/*   Updated: 2025/01/02 21:12:07 by aozkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void rrot(t_stack **first_node)
+{
+    t_stack	*last_node;
+    if (!(*first_node) || !(*first_node)->next)
+        return;
+    last_node = get_last_node(*first_node);
+    last_node->next = *first_node;
+    last_node->prev->next = NULL;
+    last_node->prev = NULL;
+    *first_node = last_node;
+    last_node->next->prev = last_node;
+}
+
 void	rra(t_stack **stack, bool disable_print)
 {
-    
+    rrot(stack);
+    if (!disable_print)
+        ft_printf("rra");
 }
 
 void	rrb(t_stack **stack, bool disable_print)
 {
-    
+    rrot(stack);
+    if (!disable_print)
+        ft_printf("rrb");
 }
 
 void	rrr(t_stack **stack_a, t_stack **stack_b, bool disable_print)
 {
-    
+    rrot(stack_a);
+    rrot(stack_b);
+    if (!disable_print)
+        ft_printf("rrr");
 }
